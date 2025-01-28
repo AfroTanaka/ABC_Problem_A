a16303b201e5fb4a806fcf3146458e1f0985c037
AfroTanaka <heapoverfl0win@icloud.com>
2025-01-28 19:26:41 +0900

    263 - 264

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
-    if (sqrt(1 * 12800000 + 1) == sqrt(12800001))
-        cout << "Yes" << endl;;
+    string s = "ONEPIECE";
+    for (int i = 0; i < 26; i++)
+        cout << (char)(s.at(0) + i) << (char)(s.at(1) + i) << (char)(s.at(2) + i) << " " << (char)(s.at(3) + i) << (char)(s.at(4) + i) << (char)(s.at(5) + i) << (char)(s.at(6) + i) << (char)(s.at(7) + i) << endl;
 }
